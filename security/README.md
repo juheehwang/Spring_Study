42e43b3b8d6d4eb47e6491e57384df69e257d4f1
juheehwang <fhqj21@gmail.com>
2025-05-21 00:24:38 +0900

    http basic 인증

--- security/README.md
+++
@@ -1,5 +0,0 @@
-### 정리본 (spring security!)
-https://juhee-judy.notion.site/Spring-Security-1f1d4295febf8039af6ac7ba2d74b5e6?pvs=4
-
-해당 베이스 강의 
-인프런
--- security/src/main/java/com/spring/security/SecurityConfig.java
+++ security/src/main/java/com/spring/security/SecurityConfig.java
@@ -1,23 +1,14 @@
 package com.spring.security;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
@@ -25,24 +16,28 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // formLogin() API version
+//        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+//            .formLogin(form -> form
+//                .loginPage("/loginPage")
+//                .loginProcessingUrl("/loginProc")
+//                .defaultSuccessUrl("/",true)
+//                .failureUrl("/failed")
+//                .usernameParameter("userId")
+//                .passwordParameter("passwd")
+//                .successHandler((request, response, authentication) -> {
+//                    System.out.println("authentication : " + authentication);
+//                    response.sendRedirect("/home");
+//                })
+//                .failureHandler((request, response, exception) -> {
+//                    System.out.println("exception : "+ exception.getMessage());
+//                    response.sendRedirect("/login");
+//                })
+//                .permitAll()
+//            );
+        // http basic version
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .formLogin(form -> form
-                .loginPage("/loginPage")
-                .loginProcessingUrl("/loginProc")
-                .defaultSuccessUrl("/",true)
-                .failureUrl("/failed")
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .successHandler((request, response, authentication) -> {
-                    System.out.println("authentication : " + authentication);
-                    response.sendRedirect("/home");
-                })
-                .failureHandler((request, response, exception) -> {
-                    System.out.println("exception : "+ exception.getMessage());
-                    response.sendRedirect("/login");
-                })
-                .permitAll()
-            );
+            .httpBasic(basic -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
         return http.build();
     }
