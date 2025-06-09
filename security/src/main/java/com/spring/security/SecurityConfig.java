1e82a1a4859fb2b65775fda6d63433f64f4039f7
juheehwang <fhqj21@gmail.com>
2025-06-10 00:18:49 +0900

    authentication provider

--- security/src/main/java/com/spring/security/SecurityConfig.java
+++ security/src/main/java/com/spring/security/SecurityConfig.java
@@ -7,7 +7,9 @@ import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
@@ -95,16 +97,17 @@ public class SecurityConfig {
 //            );
 
         // 기본
-        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-        requestCache.setMatchingRequestParameterName("customParam=y");
         http.authorizeHttpRequests(auth -> auth
             .requestMatchers("/logoutSuccess").permitAll()
             .anyRequest().authenticated())
+            .authenticationProvider(new CustomerAuthenticationProvider())
+            .authenticationProvider(new CustomerAuthenticationProvider2())
             .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
+  
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user")
